6d7ac33b34a97cf8fde8966d7e1f48122909c534
Ian <bfxzyn@gmail.com>
2013-08-29 17:49:54 -0700

    first

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  hello_world
+//
+//  Created by Ian z on 13-3-4.
+//  Copyright (c) 2013年 Ian z. All rights reserved.
+//
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    cout<<"ssss"<<endl;
+     cout<<"ssss"<<endl;
+     cout<<"ssss"<<endl;
+     cout<<"ssss"<<endl;
+    
+    return 0;
+}
+
